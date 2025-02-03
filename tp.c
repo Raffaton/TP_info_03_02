2e46de1f873fa796a2cfbb9b97ba7569c1150971
Raphael <raphael.souchet.85@gmail.com>
2025-02-03 10:21:09 +0100

    1 et 2

--- tp.c
+++ tp.c
@@ -1,1 +1,9 @@
 #include <tp.h>
+
+Note* creerNote(float duree_note, float frequence_note) {
+    Note* note1 = malloc(sizeof(Note));
+    note1->duree = duree_note;
+    note1->frequence = frequence_note;
+
+    return note1;
+}
--- tp.h
+++ tp.h
@@ -2,5 +2,13 @@
 #define TP_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
 
+typedef struct {
+    float frequence;
+    float duree;
+}Note;
+
+Note* creerNote(float duree_note, float frequence_note);
 #endif
